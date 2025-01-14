e51bc3539e1af7a0552b5575ba1ecc140d9a002b
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-14 21:52:37 +0000

    Added Longest Substring Without Repeating Characters

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -2,8 +2,8 @@
 // Solutions are not meant to be executed here, as I don't have access to test cases
 // All answers can be copy-pasted into the Solution member function definition in Leetcode
 
-// Q1: Two Sum, 11ms
-/*
+
+/*Q1: Two Sum, 11ms
 unordered_map<int,int> hash;
         for (int i=0; i<nums.size(); ++i){
             hash[nums[i]] = i; //record the values in a map so that you don't have to scour the whole list again
@@ -20,8 +20,7 @@ unordered_map<int,int> hash;
 */
 
 
-// Q2: Add Two Numbers, 27ms
-/*
+/* Q2: Add Two Numbers, 27ms
  * // form a new ListNode
         ListNode* l = new ListNode((l1->val + l2->val)%10);
         bool carry = (l1->val + l2->val) > 9 ? true : false ;
@@ -78,7 +77,39 @@ unordered_map<int,int> hash;
         return l;
 */
 
+/* Q3: Longest Substring Without Repeating Characters, 1121 ms
+ * const int& s_length = s.length();
+        if (s_length == 0) {
+            return 0;
+        }
 
+        std::set<char> unique_chars(s.begin(), s.end());
+        const int& num_uniq = unique_chars.size();
+
+        // non-repeating substrings cannot have length > num_uniq
+        // if a substring has length > num_uniq, then it has to have a repeating char
+        // pigeonhole principle
+
+        int sub_str_length = num_uniq;
+        while (sub_str_length > 1) {
+            for (int i = 0; i < (s_length - sub_str_length + 1); i++) {
+
+                std::string sub_str = s.substr(i, sub_str_length);
+
+                std::set<char> sub_unique_chars(sub_str.begin(), sub_str.end());
+                int sub_num_uniq = sub_unique_chars.size();
+
+                if (sub_num_uniq == sub_str_length) { // if num of unique chars is equal to the currently checked length of substring,
+                    return sub_str_length; // then we found the right substring
+                }
+            }
+            --sub_str_length;
+        }
+        return 1;
+ * 
+ * 
+ * 
+ * */
 
 int main() {
 
