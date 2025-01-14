a1e846c895337c33d2bbfcc3ae435d33f277e5d8
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-14 21:49:19 +0000

    Added Two Sum solution

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+
+// Solutions are not meant to be executed here, as I don't have access to test cases
+// All answers can be copy-pasted into the Solution member function definition in Leetcode
+
+// Q1: Two Sum, 11ms
+/*
+unordered_map<int,int> hash;
+        for (int i=0; i<nums.size(); ++i){
+            hash[nums[i]] = i; //record the values in a map so that you don't have to scour the whole list again
+        }
+
+        for (int i=0; i<nums.size(); ++i){
+            int diff = target - nums[i];
+            if (hash.find(diff) != hash.end() && hash[diff] != i) {
+                return {i,hash[diff]};
+            }
+        }
+
+        return {};*/
+
+
+
+
+        }*/
+
 
 int main() {
-    
-    std::cout << "Default Project Template" << std::endl;
+
 	return 0;
 }
