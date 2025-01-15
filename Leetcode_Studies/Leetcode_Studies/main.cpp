578e7378f1f6a1fa02b37abbf1afeb1273eec1be
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-15 17:57:20 +0000

    Added Q58

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -3,8 +3,22 @@
 #include <vector>
 #include <iostream>
 
-int main() {
+// binary search quick write exercise
+int binarySearch(std::vector<int>& vec, int target) {
+    
+    int left=0, right = vec.size()-1;
+    while (left <= right) {
+        int mid = left + (right - left)/2;
+        if (target == vec[mid]) return mid;
+        else if (target < vec[mid]) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+}
 
+
+int main() {
+        
     
 	return 0;
 }
