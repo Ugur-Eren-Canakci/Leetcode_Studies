531505162ef33e98c715c20a21f5ef915d6943d3
Ugur-Eren-Canakci <ugureren.canakci@gmail.com>
2025-01-19 18:03:44 +0000

    Added Q202

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -1,11 +1,32 @@
 // Solutions are not meant to be executed here, as I don't have access to test cases
 // All answers can be copy-pasted into the Solution member function definition in Leetcode
 #include <vector>
+#include <set>
 #include <iostream>
 #include <cmath>
 
+bool isHappy(int n) {
+        static std::set<int> squares;
+        for (const auto& el : squares) std::cout << el << ", ";
+        std::cout << std::endl;
+        
+        std::vector<int> digits;
+        while (n>0) {
+            digits.push_back(n%10);
+            n /= 10;
+        }
+        int square_of_digits = 0;
+        for (const auto& el : digits) square_of_digits += el*el;
+        
+        
+        if (square_of_digits == 1) return true;
+        else if (squares.insert(square_of_digits).second) return isHappy(square_of_digits);
+        else return false;
+    }
 
 int main() {
     
+    std::cout << std::boolalpha;
+    std::cout << isHappy(13) << isHappy(10);
     return 0;
 }
