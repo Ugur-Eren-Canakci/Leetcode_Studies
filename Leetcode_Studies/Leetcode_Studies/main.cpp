303de4ace055f055ee9c1c1fe4d0f512a13af87e
Ugur-Eren-Canakci <ugureren.canakci@gmail.com>
2025-01-19 17:12:16 +0000

    Added Q190

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -5,8 +5,39 @@
 #include <cmath>
 
 
-int main() {
+std::uint32_t reverseBits(uint32_t n) {
+        std::uint32_t original_number = n;
+        std::vector<int> bits;
+        while (original_number>0) {
+            bits.push_back(original_number % 2);
+            original_number /= 2;
+        }
 
+        std::uint32_t res = 0;
+        for (const auto& el : bits) {
+            res *= 2;
+            res += el;
+        }
+
+        int length_1=0, length_2=0;
+        std::uint32_t temp_1 = n, temp_2 = res;
+        while (temp_1 > 0) {
+            length_1++;
+            temp_1 /= 2; 
+        }
+        while (temp_2 > 0) {
+            length_2++;
+            temp_2 /= 2; 
+        }
+        std::cout << "temp_1: " << temp_1 << ", temp_2: " << temp_2 << std::endl;
+        for (int i=0; i<length_1 - length_2; i++) {
+            res *= 2;
+        }
+        return res;
+    }
+
+int main() {
     
+    std::cout << reverseBits(0b00000010100101000001111010011100);
     return 0;
 }
