c494f4b6f5113405adfae9b3fdae4f9c4385fe71
Ugur-Eren-Canakci <ugureren.canakci@gmail.com>
2025-01-26 16:50:51 +0000

    Added Q415, Q434, Q441

--- Leetcode_Studies/Leetcode_Studies/main.cpp
+++ Leetcode_Studies/Leetcode_Studies/main.cpp
@@ -9,40 +9,75 @@
 
 using namespace std;
 
-int longestPalindrome(string s) {
-        if (s.length() < 2) return 1;
+string addStrings(string num1, string num2) {
+        if (num1=="0") return num2;
+        if (num2=="0") return num1;
         
-        int counts[52] = {0};
-        for (char c : s) {
-            if (int(c) > 64 && int(c) < 91) counts[c - 'A']++;
-            else if (int(c) > 96 && int(c) < 123) counts[c - 'a' + 26]++;
+        // make num1 the larger/longer number
+        int num1_value = 0, num2_value = 0;
+        for (int i=0; i<num1.length(); i++) {
+            num1_value += num1[i]*pow(10,num1.length()-1-i);
         }
-
-        int num_of_unique_chars = 0;
-        for (int i=0; i<52; i++) {
-            std::cout << counts[i] << ",";
-            if (counts[i] > 0) num_of_unique_chars++;
+        for (int i=0; i<num2.length(); i++) {
+            num2_value += num2[i]*pow(10,num1.length()-1-i);
         }
-        std::cout << std::endl;
         
-        if (num_of_unique_chars == 1) return s.length();
+        if (num1_value < num2_value) {
+            string temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        
+        num1 = "0" + num1;
+        bool add_to_next = false;
 
-        int res = 0;
-        int odd_count = 0;
-        for (int i=0; i<52; i++) {
-            if (counts[i] % 2 == 0) res += counts[i];
-            else if (counts[i] % 2 == 1 && counts[i] > odd_count) {
-                odd_count = counts[i];
+        int num1_length = num1.length();
+        int num2_length = num2.length();
+
+        int it = 0;
+        while (it < num2_length) {
+            int digit_1 = int(num1[num1_length-1-it]-48);
+            int digit_2 = int(num2[num2_length-1-it]-48);
+            std::cout << "adding " << digit_1 << " and " << digit_2 << std::endl;
+            if (add_to_next) {
+                num1[num1_length-1-it] = char(((digit_1 + digit_2 + 1) % 10) + 48);
+                if (digit_1 + digit_2 + 1 > 9) add_to_next = true;
             }
+            else {
+                num1[num1_length-1-it] = char(((digit_1 + digit_2) % 10) + 48);
+                if (digit_1 + digit_2 > 9) add_to_next = true;
+            }
+            it++;
         }
-        return res + odd_count ;
+
+        while (it < num1_length) {
+            int digit_1 = int(num1[num1_length-1-it]-48);
+            if (add_to_next) {
+                num1[num1_length-1-it] = char(((digit_1 + 1) % 10) + 48);
+                if (digit_1 + 1 > 9) add_to_next = true;
+                else add_to_next = false;
+            }
+            else {
+                num1[num1_length-1-it] = char((digit_1  % 10) + 48);
+                if (digit_1 > 9) add_to_next = true;
+                else add_to_next = false;
+            }
+            it++;
+        }
+        
+        if (num1[0] == '0') {
+            string res = "";
+            for (int i=1; i<num1.size(); i++) res+=num1[i];
+            return res;
+        }
+        return num1;
+
     }
     
     
 int main() {
     
-    string s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
-    std::cout << longestPalindrome(s) << std::endl;
+    std::cout << addStrings("408","5") << std::endl;
     
     
     return 0;
